0fef6d195f30f239581635e1a4f32ef001f99f94
MrElle <cassaralavoro@gmail.com>
2024-01-24 10:22:02 +0100

    add connection with booleans

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,28 +4,28 @@
     <ul>
         @foreach($trains as $train)
             <li>
-                <p>{{ $train->agency }}</p>
+                <p><span class="fw-bold">Azienda</span> {{ $train->agency }}</p>
             </li>
             <li>
-                <p>{{ $train->departure_station }}</p>
+                <p><span class="fw-bold">Stazione di partenza:</span> {{ $train->departure_station }}</p>
             </li>
             <li>
-                <p>{{ $train->arrival_station }}</p>
+                <p><span class="fw-bold">Stazione di arrivo:</span> {{ $train->arrival_station }}</p>
             </li>
             <li>
-                <p>{{ $train->departure_time }}</p>
+                <p><span class="fw-bold">Orario di partenza:</span> {{ $train->departure_time }}</p>
             </li>
             <li>
-                <p>{{ $train->arrival_time }}</p>
+                <p><span class="fw-bold">Orario di arrivo:</span> {{ $train->arrival_time }}</p>
             </li>
             <li>
-                <p>{{ $train->train_code }}</p>
+                <p><span class="fw-bold">Codice treno:</span> {{ $train->train_code }}</p>
             </li>
             <li>
-                <p>{{ $train->in_time }}</p>
+                <p><span class="fw-bold">In orario:</span> {{ $train->in_time ? 'Si' : 'No' }}</p>
             </li>
             <li>
-                <p>{{ $train->delated }}</p>
+                <p><span class="fw-bold">Cancellato:</span> {{ $train->delated === 0 ? 'No' : 'Si' }}</p>
             </li>
         @endforeach
     </ul>
